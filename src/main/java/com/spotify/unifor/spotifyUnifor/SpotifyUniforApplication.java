f7d93a936968735575828715cfd2b54f8076adaa
joao.victor <joao.victor@f13.com.br>
2020-05-27 17:16:02 -0300

    fix

---
+++ src/main/java/com/spotify/unifor/spotifyUnifor/SpotifyUniforApplication.java
@@ -0,0 +1,13 @@
+package com.spotify.unifor.spotifyUnifor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyUniforApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyUniforApplication.class, args);
+	}
+
+}
